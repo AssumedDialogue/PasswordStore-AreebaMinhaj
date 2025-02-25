e2553d8c14979a35df74a27e71f2a9354c48841d
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2025-02-24 22:40:38 -0500

    Initial commit, with packages and sql files with the properties configured

---
+++ src/main/java/ca/sheridancollege/minhaja/passwordstoreareebaminhaj/PasswordStoreAreebaMinhajApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.minhaja.passwordstoreareebaminhaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordStoreAreebaMinhajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordStoreAreebaMinhajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/sheridancollege/minhaja/passwordstoreareebaminhaj/PasswordStoreAreebaMinhajApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.minhaja.passwordstoreareebaminhaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordStoreAreebaMinhajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
