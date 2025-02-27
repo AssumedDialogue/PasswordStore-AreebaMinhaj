5a460625c931bf72bae94b7f62bda2b2df995955
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2025-02-26 19:02:42 -0500

    added code for interface and added the imports for the home controller

--- src/main/java/ca/sheridancollege/minhaja/passwordstoreareebaminhaj/controllers/HomeController.java
+++ src/main/java/ca/sheridancollege/minhaja/passwordstoreareebaminhaj/controllers/HomeController.java
@@ -1,4 +1,22 @@
 package ca.sheridancollege.minhaja.passwordstoreareebaminhaj.controllers;
 
+import ca.sheridancollege.minhaja.passwordstoreareebaminhaj.beans.PasswordRecord;
+import ca.sheridancollege.minhaja.passwordstoreareebaminhaj.database.DatabaseAccess;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Controller
 public class HomeController {
+    private DatabaseAccess databaseAccess;
+
+    public HomeController(DatabaseAccess databaseAccess) { this.databaseAccess = databaseAccess;}
+
+
 }
--- src/main/java/ca/sheridancollege/minhaja/passwordstoreareebaminhaj/database/DatabaseAccess.java
+++ src/main/java/ca/sheridancollege/minhaja/passwordstoreareebaminhaj/database/DatabaseAccess.java
@@ -1,4 +1,13 @@
 package ca.sheridancollege.minhaja.passwordstoreareebaminhaj.database;
 
-public interface DatabaseAccess {
+import ca.sheridancollege.minhaja.passwordstoreareebaminhaj.beans.PasswordRecord;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DatabaseAccess extends CrudRepository<PasswordRecord, Long> {
+
+    List<PasswordRecord> findByTitle(String Title);
 }
